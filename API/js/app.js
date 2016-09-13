ce523f9e00d76ca0f50ee897bd33c736112a1668
Pascal Cañadas <pascal@mirandais.fr>
2016-09-13 22:28:49 +0200

    Listing of picture to watch and select to rotate

--- API/js/app.js
+++ API/js/app.js
@@ -1,3 +1,4 @@
+require('nw.gui').Window.get().showDevTools();
 /***********************************
  **
  **        Gestion du Drag and Drop
@@ -24,6 +25,20 @@ el.ondrop = function(e){
     for (var i=0; i< e.dataTransfer.files.length; ++i){
         var file= e.dataTransfer.files[i].path;
         var el = document.getElementById('carousel-inner');
-        el.innerHTML += '<img src="' + file +'" /><br />';
+        div_item = document.createElement('div');
+        div_carrousel = document.createElement('div');
+        image = document.createElement('img');
+        image.src=file;
+        image.className = "img-responsive";
+        div_carrousel.className = "carousel-page slide-" + i;
+        div_item.className= "item";
+        if(i == 0)
+        {
+            div_item.className="active";
+        }
+        div_carrousel.appendChild(image);
+        div_item.appendChild(div_carrousel);
+        el.appendChild(div_item);
     }
+    this.innerHTML= "Traitement fini";
 };
