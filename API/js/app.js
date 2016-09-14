dfc36bbd8e74cc6d6d7870490d9fcc3f89e45b6f
Pascal Cañadas <pascal@mirandais.fr>
2016-09-14 22:55:58 +0200

    Begin of action on picture
    Debut de l'ajout des fonctionnalités pour les images

--- API/js/app.js
+++ API/js/app.js
@@ -24,21 +24,36 @@ el.ondrop = function(e){
     this.innerHTML= "Traitement en cours";
     for (var i=0; i< e.dataTransfer.files.length; ++i){
         var file= e.dataTransfer.files[i].path;
-        var el = document.getElementById('carousel-inner');
-        div_item = document.createElement('div');
-        div_carrousel = document.createElement('div');
-        image = document.createElement('img');
-        image.src=file;
-        image.className = "img-responsive";
-        div_carrousel.className = "carousel-page slide-" + i;
-        div_item.className= "item";
-        if(i == 0)
-        {
-            div_item.className="active";
-        }
-        div_carrousel.appendChild(image);
-        div_item.appendChild(div_carrousel);
-        el.appendChild(div_item);
+        html = '<div class="row slide">\
+                <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12">\
+                    <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12 thumbnail">\
+                        <div class="action" style="text-align: center;height: 80px;"><br />\
+                            <button type="button" class="btn btn-default">\
+                                <span class="fa fa-1x fa-undo" aria-hidden="true">&nbsp;90°</span>\
+                            </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                            <button type="button" class="btn btn-default">\
+                                <span class="fa fa-1x fa-repeat" aria-hidden="true">&nbsp;90°</span>\
+                            </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                            <button type="button" class="btn btn-default">\
+                                <span class="fa fa-1x fa-repeat" aria-hidden="true">&nbsp;180°</span>\
+                            </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                            <button type="button" class="btn btn-default pclose">\
+                                <span class="fa fa-1x fa-times-circle-o" aria-hidden="true">&nbsp;Fermer</span>\
+                            </button>\
+                            <div class="clearfix"></div>\
+                        </div>\
+                        <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12 thumbnail">\
+                            <img src="' + file + '"/>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>';
+        ActionImage = document.getElementById('ActionImage');
+        ActionImage.innerHTML += html;
     }
     this.innerHTML= "Traitement fini";
 };
+//gère la fermeture de la photo choisi
+$("body").on("click", ".pclose", function(){
+    $(this).parent().parent().remove();
+});
